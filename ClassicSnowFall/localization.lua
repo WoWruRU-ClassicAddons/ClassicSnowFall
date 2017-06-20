51e9d8a21666f32ada9e69bde8552fc21d917191
M2us <Dead_Maus@mail.ru>
2017-06-20 19:58:57 +0300

    Added ClassicSnowFall with support ruRU locale

---
+++ ClassicSnowFall/localization.lua
@@ -0,0 +1,18 @@
+-- Version : English
+SF_SELFCAST_DISABLED = "Classic Snowfall ALT SelfCast now disabled.";
+SF_SELFCAST_ENABLED = "Classic Snowfall ALT SelfCast now enabled.";
+
+
+
+-- Version : Russian ( by Maus )
+if ( GetLocale() == "ruRU" ) then
+	SF_SELFCAST_DISABLED = "Snowfall: Применение на себя по клавише ALT – отключено.";
+	SF_SELFCAST_ENABLED = "Snowfall: Применение на себя по клавише ALT – включено.";
+end
+
+
+
+
+
+
+
